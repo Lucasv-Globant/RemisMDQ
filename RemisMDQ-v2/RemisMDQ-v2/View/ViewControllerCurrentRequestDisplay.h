f2b41532b1478f8486ec3d4c38fea99ab9d6693a
Lucasv-Globant <lucas.violini@globant.com>
2015-01-15 10:01:47 -0300

    Added blank view controllers
    Added blank view controllers

---
+++ RemisMDQ-v2/RemisMDQ-v2/View/ViewControllerCurrentRequestDisplay.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerCurrentRequestDisplay.h
+//  RemisMDQ-v2
+//
+//  Created by Lucas on 1/15/15.
+//  Copyright (c) 2015 Globant iOS MDQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerCurrentRequestDisplay : UIViewController
+
+@end
